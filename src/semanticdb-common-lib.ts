2a63411d5625c142b83c9198a7b6a1a710300f52
kilik52 <kilik52@hotmail.com>
2020-04-22 16:51:00 +0800

    "each" function

--- src/semanticdb-common-lib.ts
+++ src/semanticdb-common-lib.ts
@@ -1,6 +1,20 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
 
+/**
+ * 
+ * @param arr Array or item
+ * @param func function on each item
+ */
+const each = async (arr: any, func: Function) => {
+  if (arr === null || arr === undefined) return null;
+
+  if (Array.isArray(arr)) {
+    const result = await Promise.all(arr.map(i => func(i)));
+    return result;
+  }
+
+  return func(arr);
+};
+
+export default {
+  each,
 }
--- test/semanticdb-common-lib.test.ts
+++ test/semanticdb-common-lib.test.ts
@@ -1,14 +1,35 @@
-import DummyClass from "../src/semanticdb-common-lib"
+import _ from "../src/semanticdb-common-lib"
 
 /**
- * Dummy test
+ * test
  */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
+describe("test", () => {
+  it("each", async () => {
+    const arr = [1, 2, 3];
+    const item = 1;
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+    const nullRet = await _.each(null, (item: any) => item + 1);
+    expect(nullRet).toBeNull();
+
+    const arrRet = await _.each(arr, (item: any) => item + 1);
+    expect(arrRet).toEqual([2, 3, 4]);
+
+    const itemRet = await _.each(item, (item: any) => item + 1);
+    expect(itemRet).toEqual(2);
+
+    const asyncFunc = (item: number) => {
+      const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(item + 1);
+        }, 100);
+      });
+      return promise;
+    }
+
+    const arrRetAsync = await _.each(arr, asyncFunc);
+    expect(arrRetAsync).toEqual([2, 3, 4]);
+
+    const itemRetAsync = await _.each(item, asyncFunc);
+    expect(itemRetAsync).toEqual(2);
   })
 })
